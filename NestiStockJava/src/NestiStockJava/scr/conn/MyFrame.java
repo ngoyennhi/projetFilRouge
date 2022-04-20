ae0290b25d6b1743f7f8d7fbe5ad2252c8b7f710
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-04-20 21:18:56 +0200

    update MyFrame.java

--- NestiStockJava/src/NestiStockJava/scr/conn/MyFrame.java
+++ NestiStockJava/src/NestiStockJava/scr/conn/MyFrame.java
@@ -1,19 +1,45 @@
 package NestiStockJava.scr.conn;
 
 import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
-//Jframe is parent of Myframe
+//Jthis is parent of Mythis
 @SuppressWarnings("serial")
 public class MyFrame extends JFrame {
 	//Construction 
 	MyFrame() {
+//	this.setTitle("NestiStockJava V.1"); // sets title of this
+//	this.setDefaultCloseOperation(Jthis.EXIT_ON_CLOSE); // exit out of application
+//	this.setResizable(false); // prevent this from being resized
+//	this.setVisible(true); // make this visible
+//	this.setSize(800,800); // sets the x-dimension, and  y-dimension of this
+//	this.getContentPane().setBackground(new Color(0xCDC9C9)); // change color of background
+	
+	JLabel label = new JLabel();
+	label.setBounds(0, 0, 800, 90);
+	label.setVerticalAlignment(SwingConstants.CENTER);
+	label.setHorizontalAlignment(SwingConstants.CENTER);
+	label.setText("GESTION DE STOCK NESTI"); //set text of label
+	label.setHorizontalTextPosition(SwingConstants.CENTER); //set text CENTER 
+	label.setVerticalTextPosition(SwingConstants.TOP); //set text TOP 
+	label.setForeground(new Color(0xCDC9C9));
+	label.setBackground(new Color(0, 86, 98));;	
+	label.setOpaque(true); //display background color
+	label.setFont(new Font("Rockwell", Font.BOLD, 40));
+			
+	this.setBounds(100, 100, 450, 300); // set (x,y,weight,high) as well as dimension
 	this.setTitle("NestiStockJava V.1"); // sets title of this
 	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
 	this.setResizable(false); // prevent this from being resized
 	this.setVisible(true); // make this visible
 	this.setSize(800,800); // sets the x-dimension, and  y-dimension of this
-	this.getContentPane().setBackground(new Color(0xCDC9C9)); // change color of background	
+	this.getContentPane().setBackground(new Color(0xCDC9C9)); // change color of background
+	this.getContentPane().setLayout(null);
+	this.getContentPane().add(label);
+	//this.pack(); // to get all together after ADD all COMPONENTS - resize together 
 }
 }
--- NestiStockJava/src/NestiStockJava/scr/conn/NestiStockJava.java
+++ NestiStockJava/src/NestiStockJava/scr/conn/NestiStockJava.java
@@ -53,53 +53,30 @@ public class NestiStockJava {
 	 * Méthode initialize qui est applelé dans le constructeur
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		
-		JLabel label = new JLabel();
-		label.setBounds(0, 0, 800, 90);
-		label.setVerticalAlignment(SwingConstants.CENTER);
-		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setText("GESTION DE STOCK NESTI"); //set text of label
-		label.setHorizontalTextPosition(SwingConstants.CENTER); //set text CENTER 
-		label.setVerticalTextPosition(SwingConstants.TOP); //set text TOP 
-		label.setForeground(new Color(0xCDC9C9));
-		label.setBackground(new Color(0, 86, 98));;	
-		label.setOpaque(true); //display background color
-		label.setFont(new Font("Rockwell", Font.BOLD, 40));
-				
-		frame.setBounds(100, 100, 450, 300);
-		frame.setTitle("NestiStockJava V.1"); // sets title of frame
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // exit out of application
-		frame.setResizable(false); // prevent frame from being resized
-		frame.setVisible(true); // make frame visible
-		frame.setSize(800,800); // sets the x-dimension, and  y-dimension of frame
-		frame.getContentPane().setBackground(new Color(0xCDC9C9)); // change color of background
-		frame.getContentPane().setLayout(null);
-		frame.getContentPane().add(label);
-		
-		
+		frame = new MyFrame(); // Class MyFrame.java
+
 		//Une boite de dialogue pour configurer les informations de connexion à la base de données.
 
-		//Il s'agit d'une boîte de dialogue permettant de saisir les deux informations nécessaires à une connexion JDBC : 
-		//le login et le mot de passe.
-		JLayeredPane dialogue_conn = new JLayeredPane();
-		dialogue_conn.setForeground(Color.WHITE);
-		dialogue_conn.setBackground(Color.WHITE);
-		dialogue_conn.setBounds(106, 218, 606, 302);
-		frame.getContentPane().add(dialogue_conn);
-		dialogue_conn.setLayout(null);
-		
-		JLabel dialogue_cnn_label = new JLabel("Connexion Base de Donnée");
-		dialogue_cnn_label.setBounds(0, 0, 500, 50);
-		dialogue_cnn_label.setVerticalAlignment(SwingConstants.CENTER);
-		dialogue_cnn_label.setHorizontalAlignment(SwingConstants.CENTER);
-		dialogue_cnn_label.setHorizontalTextPosition(SwingConstants.CENTER); //set text CENTER 
-		dialogue_cnn_label.setVerticalTextPosition(SwingConstants.CENTER); //set text TOP 
-		dialogue_cnn_label.setForeground(new Color(0xCDC9C9));
-		dialogue_cnn_label.setBackground(new Color(0, 86, 98));;	
-		dialogue_cnn_label.setOpaque(true); //display background color
-		dialogue_cnn_label.setFont(new Font("Rockwell", Font.BOLD, 20));
-		dialogue_conn.add(dialogue_cnn_label);
+//		//Il s'agit d'une boîte de dialogue permettant de saisir les deux informations nécessaires à une connexion JDBC : 
+//		//le login et le mot de passe.
+//		JLayeredPane dialogue_conn = new JLayeredPane();
+//		dialogue_conn.setForeground(Color.WHITE);
+//		dialogue_conn.setBackground(Color.WHITE);
+//		dialogue_conn.setBounds(106, 218, 606, 302);
+//		frame.getContentPane().add(dialogue_conn);
+//		dialogue_conn.setLayout(null);
+//		
+//		JLabel dialogue_cnn_label = new JLabel("Connexion Base de Donnée");
+//		dialogue_cnn_label.setBounds(0, 0, 500, 50);
+//		dialogue_cnn_label.setVerticalAlignment(SwingConstants.CENTER);
+//		dialogue_cnn_label.setHorizontalAlignment(SwingConstants.CENTER);
+//		dialogue_cnn_label.setHorizontalTextPosition(SwingConstants.CENTER); //set text CENTER 
+//		dialogue_cnn_label.setVerticalTextPosition(SwingConstants.CENTER); //set text TOP 
+//		dialogue_cnn_label.setForeground(new Color(0xCDC9C9));
+//		dialogue_cnn_label.setBackground(new Color(0, 86, 98));;	
+//		dialogue_cnn_label.setOpaque(true); //display background color
+//		dialogue_cnn_label.setFont(new Font("Rockwell", Font.BOLD, 20));
+//		dialogue_conn.add(dialogue_cnn_label);
 
 	}
 }
